1988f2e5cccde7b63e7ec1ba2b0b5586b3b64f7b
M.Zeynep <melikezeynep_97@hotmail.com>
2024-10-09 07:16:14 +0300

    Bouncing Ball animation has improved

--- src/MelikeZeynepCakmakci.java
+++ src/MelikeZeynepCakmakci.java
@@ -4,53 +4,57 @@ public class MelikeZeynepCakmakci {
 
     public static void main(String[] args) {
 
-        StdDraw.setCanvasSize(1200, 800); // set the size of the drawing canvas
-        StdDraw.setXscale(-1.5, 1.5); // set the scale of the coordinate system
+        StdDraw.setCanvasSize(1200, 800); // Set the size of the drawing canvas
+        StdDraw.setXscale(-1.5, 1.5); // Set the scale of the coordinate system
         StdDraw.setYscale(-1.0, 1.0);
         StdDraw.enableDoubleBuffering(); // Use for faster animations
+        int pauseDuration = 15; // Pause duration in milliseconds
 
-        int pauseDuration = 15; // pause duration in milliseconds (How fast is the ball)
-
-        double position_x = 0, position_y = -0.98;  // initial (x,y) ball position
+        double positionX = 0, positionY = -0.98;  // Initial (x,y) ball position
 
         Random random = new Random();
         int velocityAngle = random.nextInt(180);
         double velocityMagnitude = 0.03;
-        double velocity_x = velocityMagnitude * Math.cos(velocityAngle);
-        double velocity_y = velocityMagnitude * Math.sin(velocityAngle);  // initial velocity components
-        double radius = 0.05;  // radius of the ball
+        double velocityX = velocityMagnitude * Math.cos(Math.toDegrees(velocityAngle));
+        double velocityY = velocityMagnitude * Math.sin(Math.toDegrees(velocityAngle));
+        double radius = 0.05;  // Radius of the ball
 
-        double boxPositionX = 0.0;
-        double boxPositionY = 0.0;
+        // The components of the Box
+        double boxPositionX = 0.0, boxPositionY = 0.0; // (x,y) Box position
+        double widthLength = 0.5, heightLength = 0.2; // The length of the sides
 
-        double widthLength = 0.5;
-        double heightLength = 0.2;
+        while (true) { // Main animation loop
+            // Bounce off wall according to law of elastic collision
+            if (Math.abs(positionX + velocityX) > 1.5 - radius) //When hit right or left
+                velocityX = -velocityX;
+            if (Math.abs(positionY + velocityY) > 1.0 - radius) //When hit bottom or up
+                velocityY = -velocityY;
 
-        while (true) { // main animation loop
-            // bounce off wall according to law of elastic collision
-            if (Math.abs(position_x + velocity_x) > 1.5 - radius) //When hit right or left
-                velocity_x = -velocity_x;
-            if (Math.abs(position_y + velocity_y) > 1.0 - radius) //When hit bottom or up
-                velocity_y = -velocity_y;
+            // Bounce off box according to law of elastic collision
+            if (Math.abs(positionY + velocityY - boxPositionY) < heightLength / 2 + radius
+                    && Math.abs(positionX + velocityX - boxPositionX) < widthLength / 2 + radius) {
 
-            if (Math.abs(position_y + velocity_y - boxPositionY) < heightLength / 2 + radius
-                    && Math.abs(position_x + velocity_x - boxPositionX) < widthLength / 2 + radius) {
-                if (Math.abs(position_y + velocity_y - boxPositionY) < heightLength / 2 + radius / 2)
-                    velocity_x = -velocity_x;
-                else
-                    velocity_y = -velocity_y;
+                double overlapX = Math.abs(positionX + velocityX - boxPositionX) - widthLength / 2;
+                double overlapY = Math.abs(positionY + velocityY - boxPositionY) - heightLength / 2;
+
+                // Check where the hit occurs
+                if (overlapX > overlapY) {
+                    velocityX = -velocityX; // Reverse X velocity
+                } else {
+                    velocityY = -velocityY; // Reverse Y velocity
+                }
             }
 
-            position_x = position_x + velocity_x; // update positions
-            position_y = position_y + velocity_y;
+            positionX = positionX + velocityX; // Update x position
+            positionY = positionY + velocityY; // Update y position
 
-            StdDraw.clear(StdDraw.WHITE); // clear the background - Background is White
-            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.clear(StdDraw.WHITE); // Clear the background
+            StdDraw.setPenColor(StdDraw.BLUE); // To draw blue box
             StdDraw.filledRectangle(boxPositionX, boxPositionY, widthLength / 2, heightLength / 2);
-            StdDraw.setPenColor(StdDraw.BLACK); // draw ball on the screen - Black pen to draw ball
-            StdDraw.filledCircle(position_x, position_y, radius); //Black ball
-            StdDraw.show(); // show the drawing on the screen
-            StdDraw.pause(pauseDuration); // pause the drawing at each iteration
+            StdDraw.setPenColor(StdDraw.BLACK); // To draw black ball on the screen
+            StdDraw.filledCircle(positionX, positionY, radius);
+            StdDraw.show(); // Show the drawing on the screen
+            StdDraw.pause(pauseDuration); // Pause the drawing at each iteration
         }
     }
 }
